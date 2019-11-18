076a5388fead1904ab1c47cbc470ac85651c5e18
unknown <quentin@bonaventure.xyz>
2019-11-18 05:18:44 +0100

    Adds dependency oauth2

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule StreamHeda.MixProject do
   def application do
     [
       mod: {StreamHeda.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :oauth2]
     ]
   end
 
@@ -42,7 +42,8 @@ defmodule StreamHeda.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:oauth2, "~> 2.0"}
     ]
   end
 
